9ea23297da39c32c62006efbe41120e855778cd4
Alexander Rühl <alexander.ruehl@sti.valantic.com>
2025-10-21 14:33:22 +0200

    Empty test and wait for further implementation

--- src/test/java/com/vsti/ChatResourceTest.java
+++ src/test/java/com/vsti/ChatResourceTest.java
@@ -8,24 +8,5 @@ import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 class ChatResourceTest {
-    @Test
-    void testIndexEndpoint() {
-        given()
-          .when().get("/")
-          .then()
-             .statusCode(200)
-             .contentType("text/html");
-    }
-
-    @Test
-    void testChatEndpoint() {
-        given()
-          .formParam("message", "test message")
-          .when().post("/chat")
-          .then()
-             .statusCode(200)
-             .contentType("text/html")
-             .body(containsString("You said: test message"));
-    }
-
+ 
 }
